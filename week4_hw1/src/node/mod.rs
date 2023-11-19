d225ca7c8ea3c6e1c2732f10c23e68bfa365eded
Ivantung <ypt0708@gmail.com>
2023-11-19 15:43:03 +0800

    node finished

--- week4_hw1/src/node/mod.rs
+++ week4_hw1/src/node/mod.rs
@@ -16,26 +16,48 @@ pub struct Node<T> {
 #[allow(dead_code, unused_variables)]
 impl<T> Node<T> {
     pub fn new(data: T) -> Node<T> {
-        todo!()
+        let v: Vec<NodeRef<T>> = Vec::new();
+        Node {
+            data: data,
+            parent: None,
+            children: v,
+        }
     }
 }
 
 #[allow(dead_code, unused_variables)]
 pub fn new<T>(data: T) -> NodeRef<T> {
-    todo!()
+    let v:Vec<NodeRef<T>> = Vec::new();
+    let node = Node {
+        data: data,
+        parent: None,
+        children: v,
+    };
+    Rc::new(RefCell::new(node))
 }
 
 #[allow(dead_code, unused_variables)]
 pub fn append_child<T>(node: &NodeRef<T>, child: &NodeRef<T>) {
-    todo!()
+    child.borrow_mut().parent = Some(Rc::downgrade(&node));
+    node.borrow_mut().children.push(Rc::clone(child));
 }
 
 #[allow(dead_code, unused_variables)]
 pub fn get_child<T>(node: &NodeRef<T>, index: usize) -> Option<NodeRef<T>> {
-    todo!()
+    let child = node.borrow().children.get(index);
+    let new_rc = match child {
+        None => None,
+        Some(T) => Some(Rc::clone(child.unwrap())),
+    };
+    new_rc
 }
 
 #[allow(dead_code, unused_variables)]
 pub fn get_parent<T>(node: &NodeRef<T>) -> Option<NodeRef<T>> {
-    todo!()
+    let node_parent = node.borrow().parent;
+    let new_rc = match node_parent {
+        None => None,
+        Some(T) => node_parent.unwrap().upgrade(),
+    };
+    new_rc
 }
